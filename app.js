9d5adb1efb41d06391679fd2aefac838db4d60ef
fredy.siswanto <fredy.siswanto@komunal.co.id>
2025-03-02 04:27:19 +0700

    Refactor code formatting for consistency and readability in config and app files

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ dotenv.config();
 
 // * Authenticate Database connection
 db.sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Connection has been established successfully.');
-	})
-	.catch((err) => {
-		console.error('Unable to connect to the database:', err);
-	});
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // * Sync Database tables
 if (process.env.ALLOW_SYNC === 'true') {
-	db.sequelize.sync({ alter: true }).then(() => console.log('Done adding/updating the database based on the Models.'));
+  db.sequelize
+    .sync({ alter: true })
+    .then(() =>
+      console.log('Done adding/updating the database based on the Models.')
+    );
 }
 
 // * Initialize express
@@ -32,36 +36,48 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(
-	express.urlencoded({
-		extended: true,
-	})
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 // * Cors
 app.use(cors());
 
 const authenticateToken = (req, res, next) => {
-	const authHeader = req.headers['authorization'];
-	const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-	if (token == null) return res.sendStatus(401);
+  if (token == null) return res.sendStatus(401);
 
-	jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-		if (err) return res.sendStatus(403);
-		req.user = user;
-		next();
-	});
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 };
 
 app.use('/public', express.static(path.join(__dirname + '/public/uploads/')));
 app.use(`${process.env.API_VERSION}/home`, require('./src/routes/home.routes'));
 app.use(`${process.env.API_VERSION}/test`, require('./src/routes/test.routes'));
-app.use(`${process.env.API_VERSION}/resto-admin`, authenticateToken, require('./src/routes/resto_admin.routes'));
-app.use(`${process.env.API_VERSION}/admin`, authenticateToken, require('./src/routes/admin.routes'));
-app.use(`${process.env.API_VERSION}/customer`, authenticateToken, require('./src/routes/customer.routes'));
+app.use(
+  `${process.env.API_VERSION}/resto-admin`,
+  authenticateToken,
+  require('./src/routes/resto_admin.routes')
+);
+app.use(
+  `${process.env.API_VERSION}/admin`,
+  authenticateToken,
+  require('./src/routes/admin.routes')
+);
+app.use(
+  `${process.env.API_VERSION}/customer`,
+  authenticateToken,
+  require('./src/routes/customer.routes')
+);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 // TODO: Generate Token
